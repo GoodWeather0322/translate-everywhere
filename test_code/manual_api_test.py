21e152d70778c9ec6730f8ff5513f95cceb381c7
goodweather0322 <a82521112@gmail.com>
2024-05-20 18:30:22 +0800

    📝 docs: test code

--- test_code/manual_api_test.py
+++ test_code/manual_api_test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://localhost:55699/v1/end2end/speech-translate"
-file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/test_code/test.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240520181309.wav"
 save_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/test_code/test_return.wav"
 
 with open(file_path, "rb") as file:
