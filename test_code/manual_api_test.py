17aea5e2e0a95f5bd67a6e66e6a6975d074d8d50
goodweather0322 <a82521112@gmail.com>
2024-05-23 13:50:22 +0800

    ✅ test: 修改測試code

--- test_code/manual_api_test.py
+++ test_code/manual_api_test.py
@@ -1,16 +1,31 @@
 import requests
+import base64
 
 url = "http://localhost:55699/v1/end2end/speech-translate"
-file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240520181309.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240521/20240521132110.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240522/20240522112311.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240522/20240522170117.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240522/20240522112516.wav"
+file_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/uploaded_audio/20240522/20240522163804.wav"
 save_path = "/mnt/disk1/chris/uaicraft_workspace/translate-everywhere/test_code/test_return.wav"
 
 with open(file_path, "rb") as file:
-    response = requests.post(url, files={"file": file})
+    data = {"source_lang": "zh", "target_lang": "en"}
+    files = {"file": file}
+    response = requests.post(url, files=files, data=data)
 
 if response.status_code == 200:
     # 将返回的文件内容保存到本地
-    with open(save_path, "wb") as save_file:
-        save_file.write(response.content)
-    print(f"File saved successfully to {save_path}")
+    data = response.json()
+    print(data['source_text'])
+    print(data['target_text'])
+    if data['file'] is not None:
+        print(len(data['file']))
+        file_bytes = base64.b64decode(data['file'])
+        with open(save_path, 'wb') as f:
+            f.write(file_bytes)
+    else:
+        print("No file returned")
+
 else:
     print(f"Failed to upload file: {response.status_code}, {response.text}")
