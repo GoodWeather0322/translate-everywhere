0509d1c719d400f006a23f10a3309509175823e8
goodweather0322 <a82521112@gmail.com>
2024-05-25 13:48:33 +0800

    ✨ feat: 新增 api for rvc voice conversion

--- api/api_v1/end2end.py
+++ api/api_v1/end2end.py
@@ -73,3 +73,54 @@ async def speech_translate(file: UploadFile, source_lang: str = Form(...), targe
         "file": encoded_file
     })
 
+@router.post("/speech-translate-custom")
+async def speech_translate_custom(file: UploadFile, source_lang: str = Form(...), target_lang: str = Form(...), name: str = Form(...)):
+    """
+    This function handles the speech translation endpoint with a custom name parameter.
+    
+    Parameters form input:
+    - file: Uploaded audio file
+    - source_lang: Source language for translation
+    - target_lang: Target language for translation
+    - name: Custom name for the translation session ['evonne', 'laura']
+    
+    Returns:
+    JSONResponse with source text [source_text], target text[target_text], and base64 audio file [file]
+    """
+    logger.info("speech-translate endpoint connected!")
+    logger.info(f"source_lang: {source_lang}")
+    logger.info(f"target_lang: {target_lang}")
+    logger.info(f"name: {name}")
+    logger.info(f"*****************")
+    
+    save_path = Path("uploaded_audio")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    date = timestamp[:8]
+    save_path = save_path / date
+    save_path.mkdir(parents=True, exist_ok=True)
+    file_extension = Path(file.filename).suffix
+    file_name = f"{timestamp}{file_extension}"
+
+    async with aiofiles.open(
+        save_path / file_name, "wb"
+    ) as out_file:
+        # Read the file in chunks and write to the output file
+        while content := await file.read(1024):
+            await out_file.write(content)
+
+    source_text, target_text, output_file = await asyncio.to_thread(model.end2end_flow, source_lang, target_lang, str(save_path / file_name), name)
+    logger.info(source_text)
+    logger.info(target_text)
+
+    if output_file is not None:
+        with open(output_file, "rb") as file:
+            encoded_file = base64.b64encode(file.read()).decode("utf-8")
+    else:
+        encoded_file = None
+    # 返回 WAV 檔案和文本
+    return JSONResponse(content={
+        "source_text": source_text,
+        "target_text": target_text,
+        "file": encoded_file
+    })
+
